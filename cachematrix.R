283a38b979e76e7d685a05ac19445c9539eacad5
Geoff Moody <geoff.moody@gmail.com>
2017-11-09 13:31:33 -0500

    summary
    Project assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+xx <- makeInverse (matrix(c(1,2,3,4),2,2))
+##The inverse hasn't been calculated at this point
+xx$getsolve()
+## NULL
 }
 
 
@@ -12,4 +15,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cachesolve(xx) #inverse will be returned
+        ##       [,1] [,2]
+        ## [1,]   -2   1.5
+        ## [2,]   1    -0.5
+        cachesolve(XX) #inverse gets returned from cache
+        ##retrieves inverse
+        ##      [,1]   [,2]
+        ## [1,]  -2     1.5
+        ## [2,]   1    -0.5
+        xx$set(xx$getsolve())     #sets function call as computed inverse
+        cachesolve(XX)
+        ##       [,1]   [,2]
+        ## [1,]   1       3
+        ## [2,]   2       4
 }
+
